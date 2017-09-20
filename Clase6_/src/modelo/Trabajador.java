e5548bb62d8e86328cb8bea1bedc531ae70f2b42
borboton <ariel88lorenzo@gmail.com>
2017-09-20 00:35:03 -0300

    tres objetos tres turnos

--- Clase6_/src/modelo/Trabajador.java
+++ Clase6_/src/modelo/Trabajador.java
@@ -13,21 +13,23 @@ public class Trabajador {
 		this.edad=edad;
 	}
 	
-	public Trabajador(String nombre, int edad, int peso, Turno turno) {
+	public Trabajador(String nombre, int edad, int peso, Turno turno, double salario) {
 		this.nombre = nombre;
 		this.edad = edad;
 		this.peso = peso;
 		this.turno = turno;
+		this.salario = salario;
+		this.valorTurno(turno);
 	}
 
-	public Trabajador(String nombre2, int edad2, int peso2, Turno turno2, double salario2) {
-		this.nombre = nombre2;
-		this.edad = edad2;
-		this.peso = peso2;
-		this.turno = turno2;
-		this.salario=salario2;
-		this.valorTurno(turno2);
-	}
+//	public Trabajador(String nombre2, int edad2, int peso2, Turno turno2, double salario2) {
+//		this.nombre = nombre2;
+//		this.edad = edad2;
+//		this.peso = peso2;
+//		this.turno = turno2;
+//		this.salario=salario2;
+//		this.valorTurno(turno2);
+//	}
 
 	public String getNombre() {
 		return nombre;
@@ -68,15 +70,15 @@ public class Trabajador {
 	public void valorTurno(Turno turno) {
 		switch (turno) {
 		case MAÑANA:
-			this.salario = 1000.0;
+			//this.salario = salario;
 			break;
 
 		case TARDE:
-			this.salario = (this.salario * 1.8);
+			this.salario = (this.salario * 18 / 100 + this.salario);
 			break;
 			
 		case NOCHE:
-			this.salario = (this.salario * 2);
+			this.salario = (this.salario * 20 / 100 + this.salario);
 			break;
 		}
 	}
--- Clase6_/src/vista/Main.java
+++ Clase6_/src/vista/Main.java
@@ -8,24 +8,33 @@ import modelo.Turno;
 public class Main {
 
 	public static void main(String[] args) {
-
-
-		Trabajador t1 = new Electricista("jose", 23, 80, Turno.NOCHE, 10000);
-		Trabajador t2 = new Albañil("Juan", 35, 75, Turno.TARDE, 8500);
+		
+		Trabajador t1 = new Electricista("Jose", 23, 80, Turno.NOCHE, 10000);
+		Trabajador t2 = new Albañil("Juan", 35, 75, Turno.TARDE, 10000);
+		Trabajador t3 = new Electricista("Marcelo", 32, 88, Turno.MAÑANA,10000 );
 		
 		System.out.println("Trabajador: " + t1.getClass());
 		System.out.println("Nobre: " + t1.getNombre());
 		System.out.println("Edad: " + t1.getEdad());
 		System.out.println("Turno: " + t1.getTurno());
-		System.out.println("Salario x turno: " + t1.getSalario());
+		System.out.println("Salario: " + t1.getSalario());
 		System.out.println();
 		
 		System.out.println("Trabajador: " + t2.getClass());
 		System.out.println("Nobre: " + t2.getNombre());
 		System.out.println("Edad: " + t2.getEdad());
 		System.out.println("Turno: " + t2.getTurno());
-		System.out.println("Salario x turno: " + t2.getSalario());
+		System.out.println("Salario : " + t2.getSalario());
 		System.out.println();
+		
+
+		System.out.println("Trabajador: " + t3.getClass());
+		System.out.println("Nobre: " + t3.getNombre());
+		System.out.println("Edad: " + t3.getEdad());
+		System.out.println("Turno: " + t3.getTurno());
+		System.out.println("Salario : " + t3.getSalario());
+		System.out.println();
+		
 		
 	}
 
